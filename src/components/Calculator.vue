6d3fa6d4eff39f71aa020a579501c4d74d61226c
Katrina Fuller <trinafuller@att.net>
2019-09-23 18:46:26 -0600

    all set up to start calculator

---
+++ src/components/Calculator.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="calculator container-fluid"></div>
+</template>
+
+
+<script>
+export default {
+  name: "calculator",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
